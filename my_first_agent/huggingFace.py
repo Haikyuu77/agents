ca1b0be913a31acff9e115f7fad4b384f215e9e5
Haikyuu77 <62696931+Haikyuu77@users.noreply.github.com>
2025-01-18 12:43:15 -0800

    integrating Models

---
+++ my_first_agent/huggingFace.py
@@ -0,0 +1,10 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
+
+if __name__ == '__main__':
+    pipe(messages)
